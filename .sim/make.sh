c0fbba3f428561a9784126019a61722990ffde65
jeksterslab <learn.jeksterslab@gmail.com>
2025-01-14 16:15:59 +0000

    Automated build.

--- .sim/make.sh
+++ .sim/make.sh
@@ -6,14 +6,14 @@
 #SBATCH --output=make.out
 #SBATCH --error=make.err
 
-cp /scratch/ibp5092/sif/docs.sif /scratch/ibp5092/manCTMed_temp/.sif
+# cp /scratch/ibp5092/sif/docs.sif /scratch/ibp5092/manCTMed_temp/.sif
 # rm /scratch/ibp5092/manCTMed_temp/.setup/latex/figures/png/*.png
 
 cd /scratch/ibp5092/manCTMed_temp
-apptainer exec /scratch/ibp5092/manCTMed_temp/.sif/docs.sif make all
+# apptainer exec /scratch/ibp5092/manCTMed_temp/.sif/docs.sif make all
 
 # remake
 
-cp /scratch/ibp5092/manCTMed_temp/vignettes/*.png /scratch/ibp5092/manCTMed_temp/.setup/latex/figures/png
-apptainer exec /scratch/ibp5092/manCTMed_temp/.sif/docs.sif make all
+# cp /scratch/ibp5092/manCTMed_temp/vignettes/*.png /scratch/ibp5092/manCTMed_temp/.setup/latex/figures/png
+# apptainer exec /scratch/ibp5092/manCTMed_temp/.sif/docs.sif make all
 apptainer exec /scratch/ibp5092/manCTMed_temp/.sif/docs.sif make auto
